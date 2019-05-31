d8d2706bdb7b24e3b63854ae0c244a3800c82f1d
Lucas Souza <lucassouzaufpa@gmail.com>
2019-05-31 17:18:01 -0300

    Adiciona operção logaritmica

--- test/test_core.py
+++ test/test_core.py
@@ -35,6 +35,10 @@ class TestMethods(unittest.TestCase):
     def test_avg(self):
         operation = Operations([1,2,3,4], False)
         self.assertAlmostEqual(operation.avg(), 2.5, msg='Incorrect result')
+    
+    def test_log(self):
+        operation = Operations(100,10)
+        self.assertEqual(operation.log(), 2.0, msg='Incorrect result')
 
 if __name__ == '__main__':
     unittest.main()
